b4e2685fe92e9da819de0f76b4072e7babcc4858
Arif Rahman Hakim <16.9025@stis.ac.id>
2020-05-03 06:54:19 +0700

    Update version 1.0
    No need Callback on Server

---
+++ sample/src/main/java/dev/blank/sampleoauthstis/Constant.java
@@ -0,0 +1,7 @@
+package dev.blank.sampleoauthstis;
+
+class Constant {
+    static String CLIENT_ID = "X";
+    static String CLIENT_SECRET = "XXXXXXXX";
+    static String REDIRECT_URI = "http://XX.XX.XXX/xxx/xxx/xxx";
+}
